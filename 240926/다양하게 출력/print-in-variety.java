316481250bae224e06931cccf0f1ad339b6cac79
meengi <83638209+meengi07@users.noreply.github.com>
2024-09-26 17:32:38 +0900

    Title: 다양하게 출력, Time: 88ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240926/다양하게 출력/print-in-variety.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        String[] arr = new String[]{"Total days in Year", "365", "Circumference rate", "3.1415926535"};
+        for (String str : arr) {
+            System.out.println(str);
+        }
+    }
+}
